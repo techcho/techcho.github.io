5055110d38979c3793367ca5118f7b3516efe7e4
techcho <106016666+techcho@users.noreply.github.com>
2022-06-07 20:29:41 +0900

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,10 @@
+	function zoomIn(event) {
+    event.target.style.width = "480px";
+    event.target.style.height = "300px";
+    event.target.style.transition = "all 0.5s";
+	}
+	function zoomOut(event) {
+    event.target.style.width = "320px";
+    event.target.style.height = "240px";
+    event.target.style.transition = "all 0.5s";
+  }
